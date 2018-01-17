d33a8ce714f3e0fc4f4c35a98530bd1e2f100dc3
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-01-17 13:40:45 -0600

    modify ctrl func

--- local/src/app.js
+++ local/src/app.js
@@ -34,8 +34,28 @@ obtain(obtains, ({ fileServer, router, express }, { wss }, { config }, { zeroPad
       wss.send(i, {
         audioFile: `http://sensor-server.net/audio/track-${zeroPad(i, 2)}.mp3`,
         startPlayTime: startPlayTime,
-        controlFunc: ((point, el)=> {
-          console.log(point);
+        controlFunc: ((swing, audio)=> {
+          var swingDist = Math.sqrt(Math.pow(swing.point.x, 2) + Math.pow(swing.point.y, 2));
+          if (swingDist > .10 && !swing.high && Date.now() - swing.lastHigh > 100 && swing.totalWeight() > 10) {
+            swing.lastHigh = Date.now();
+            if (!swing.active) {
+              swing.active = true;
+              audio.rampUp();
+              console.log('rampUp');
+            }
+
+            swing.high = true;
+          } else if (swingDist < .1) {
+            swing.high = false;
+          }
+
+          if ((Date.now() - swing.lastHigh > 5000 || swing.totalWeight() < 10) && swing.active) {
+            swing.active = false;
+            audio.rampDown();
+            console.log('rampDown');
+          }
+
+          if (!swing.lastHigh) swing.lastHigh = Date.now();
         }).toString(),
       });
     });
