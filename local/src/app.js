88b7f027a657c5a178de728a7b185608dafce292
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-01-18 08:11:48 -0600

    changed audio config methods

--- local/src/app.js
+++ local/src/app.js
@@ -29,34 +29,55 @@ obtain(obtains, ({ fileServer, router, express }, { wss }, { config }, { zeroPad
 
   var startPlayTime = Date.now();
 
+  var ctrlFunc = (swing, audio)=> {
+    var swingDist = Math.sqrt(Math.pow(swing.point.x, 2) + Math.pow(swing.point.y, 2));
+    if (swingDist > .10 && !swing.high && Date.now() - swing.lastHigh > 100 && swing.totalWeight() > 10) {
+      swing.lastHigh = Date.now();
+      if (!swing.active) {
+        swing.active = true;
+        audio[0].rampUp();
+        console.log('rampUp');
+      }
+
+      swing.high = true;
+    } else if (swingDist < .1) {
+      swing.high = false;
+    }
+
+    if ((Date.now() - swing.lastHigh > 5000 || swing.totalWeight() < 10) && swing.active) {
+      swing.active = false;
+      audio[0].rampDown();
+      console.log('rampDown');
+    }
+
+    if (!swing.lastHigh) swing.lastHigh = Date.now();
+  };
+
+  var setupFunc = (track, num, arr)=> {
+    track.volume = 1;
+    track.rampTime = 2;
+    track.rampUp = ()=> {
+      track.volume = Math.min(1, Math.max(0, track.volume + .01));
+      clearTimeout(track.ramperTO);
+      if (track.volume < 1) track.ramperTO = setTimeout(track.rampUp, track.rampTime * 10);
+    };
+
+    track.rampDown = ()=> {
+      track.volume = Math.min(1, Math.max(0, track.volume - .01));
+      clearTimeout(track.ramperTO);
+      if (track.volume > 0) track.ramperTO = setTimeout(track.rampDown, track.rampTime * 10);
+    };
+  };
+
   for (let i = 0; i < 6; i++) {
     wss.onOrderedConnect(i, ()=> {
       wss.send(i, {
-        audioFile: `http://172.91.0.1/audio/track-${zeroPad(i, 2)}.mp3`,
+        audioConfig: {
+          tracks: [`http://172.91.0.1/audio/track-${zeroPad(i, 2)}.mp3`],
+          ctrlFunc: ctrlFunc.toString(),
+          setupFunc: setupFunc.toString(),
+        },
         startPlayTime: startPlayTime,
-        controlFunc: ((swing, audio)=> {
-          var swingDist = Math.sqrt(Math.pow(swing.point.x, 2) + Math.pow(swing.point.y, 2));
-          if (swingDist > .10 && !swing.high && Date.now() - swing.lastHigh > 100 && swing.totalWeight() > 10) {
-            swing.lastHigh = Date.now();
-            if (!swing.active) {
-              swing.active = true;
-              audio.rampUp();
-              console.log('rampUp');
-            }
-
-            swing.high = true;
-          } else if (swingDist < .1) {
-            swing.high = false;
-          }
-
-          if ((Date.now() - swing.lastHigh > 5000 || swing.totalWeight() < 10) && swing.active) {
-            swing.active = false;
-            audio.rampDown();
-            console.log('rampDown');
-          }
-
-          if (!swing.lastHigh) swing.lastHigh = Date.now();
-        }).toString(),
       });
     });
   }
